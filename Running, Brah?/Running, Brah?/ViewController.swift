bc2de3f71c52ea8b6616347286b89bddae49234d
Stacy K <stacykam@Stacys-MacBook-Pro.local>
2016-10-15 22:35:03 -0400

    New Release

--- Running, Brah?/Running, Brah?/ViewController.swift
+++ Running, Brah?/Running, Brah?/ViewController.swift
@@ -7,13 +7,8 @@
 //
 
 import UIKit
-import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
-    
-    var locationman: CLLocationManager!
-    var destLocation: CLLocation!
-    var currentUserLocation: CLLocation?
+class ViewController: UIViewController {
 
     @IBOutlet weak var inputMiles: UITextField!
     @IBOutlet weak var totalMiles: UILabel!
@@ -37,32 +32,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        locationman = CLLocationManager()
-        locationman.delegate = self
-        locationman.desiredAccuracy = kCLLocationAccuracyBest
-        locationman.requestAlwaysAuthorization()
-        locationman.startUpdatingLocation()
-        
-        
-    }
-    
-    func startDestLocation(userloc: CLLocation) {
-        destLocation = userloc
-    }
-    
-    
-
-    func updateLocation(){
-        currentUserLocation = locationman.location
-    }
-    
-    func getCurrentlocation() ->(Double, Double) {
-        var x_coordinate: Double
-        var y_coordinate: Double
-        x_coordinate = locationman.location!.coordinate.longitude
-        y_coordinate = locationman.location!.coordinate.latitude
-        
-        return (x_coordinate, y_coordinate)
     }
 
 
