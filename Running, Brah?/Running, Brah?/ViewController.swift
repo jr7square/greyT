7b9cdd4a955d41174b7354b3a917d30b117c001d
Stacy Kam <stacykam@Stacys-MacBook-Pro-6.local>
2016-10-15 21:12:08 -0400

    Adding xcode update

--- Running, Brah?/Running, Brah?/ViewController.swift
+++ Running, Brah?/Running, Brah?/ViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationman: CLLocationManager!
+    var destLocation: CLLocation!
+    var currentUserLocation: CLLocation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,32 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        locationman = CLLocationManager()
+        locationman.delegate = self
+        locationman.desiredAccuracy = kCLLocationAccuracyBest
+        locationman.requestAlwaysAuthorization()
+        locationman.startUpdatingLocation()
+        
+        
+    }
+    
+    func startDestLocation(userloc: CLLocation) {
+        destLocation = userloc
+    }
+    
+    
+
+    func updateLocation(){
+        currentUserLocation = locationman.location
+    }
+    
+    func getCurrentlocation() ->(Double, Double) {
+        var x_coordinate: Double
+        var y_coordinate: Double
+        x_coordinate = locationman.location!.coordinate.longitude
+        y_coordinate = locationman.location!.coordinate.latitude
+        
+        return (x_coordinate, y_coordinate)
     }
 
 
